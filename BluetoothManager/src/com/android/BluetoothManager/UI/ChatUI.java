3b629231fa5bf33469ca35354113d848797f607a
Arihant <forever.arihant@gmail.com>
2012-02-26 00:37:06 +0530

    ChatUI done. Added UI Stub for testing ChatUI.

--- BluetoothManager/src/com/android/BluetoothManager/UI/ChatUI.java
+++ BluetoothManager/src/com/android/BluetoothManager/UI/ChatUI.java
@@ -1,31 +1,30 @@
 package com.android.BluetoothManager.UI;
 
-
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 
+import com.android.BluetoothManager.Application.BluetoothManagerApplication;
 import com.android.BluetoothManager.UI.viewpager.TitlePageIndicator;
 
 public class ChatUI extends Activity {
-	
+
 	ViewPagerAdapter adapter;
 	ViewPager pager;
 	TitlePageIndicator indicator;
-	
-	
+	BluetoothManagerApplication bluetooth_manager;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.chat_ui);
-		adapter = new ViewPagerAdapter(this);
+		bluetooth_manager = (BluetoothManagerApplication) getApplication();
+		adapter = bluetooth_manager.ui_packet_receiver.adapter;
 		indicator = (TitlePageIndicator) findViewById(R.id.indicator);
 		pager = (ViewPager) findViewById(R.id.viewpager);
-		
+		bluetooth_manager.ui_packet_receiver.adapter.context = this;
 		pager.setAdapter(adapter);
 		indicator.setViewPager(pager);
-		
-		adapter.addNewView(pager,null);
 		adapter.notifyDataSetChanged();
 		indicator.notifyDataSetChanged();
 	}
